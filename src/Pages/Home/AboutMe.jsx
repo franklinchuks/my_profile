5a5ebf6aee810ffc9c8befd8179ed2ee469f252c
franklin <franklinchukwuemeka234@gmail.com>
2024-08-13 20:59:08 +0300

    first commit 2

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -15,12 +15,12 @@ export default function AboutMe() {
                     <p className="text-md">
                         Currently, I put together a strong analytical skillset, in-depth knowledge of financial markets, and proficiency in programming languages to develop quantitative strategies. My experience includes building profitable algorithmic bots for the commodities market at QNC, 3 of which were based on my own strategies. These strategies deliver consistent returns, with even the lowest performer netting at least 25% annual ROI, a Sharpe Ratio of 0.7, and a maximum drawdown of 15% (depending on lot size).
                     </p>
-                    <Link className="btn btn-outline-primary">Resume</Link>
+                    <a href="./doc/cv.pdf" target="_blank"><button className="btn btn-primary">View my resume</button></a>
                 </div>
             </div>
 
             <div className="portfolio--container">
-                <h1 className="section--heading">Hobbies</h1>
+                <h2 className="section--heading">Hobbies</h2>
             </div>
         </section>
     );
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -4,16 +4,17 @@ export default function HeroSection() {
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
                     <p className="section--title">
-                        Hey, I'm Franklin...
+                        Hey, I'm Franklin...<br /> <br /> 
                     </p>
                     <h1 className="hero--section--title">
                         <span className="hero--section--title--color">Quant</span>{" "} and a
                         <br />
                         Software Developer
+                        <br /> 
                     </h1>
                     <p className="hero--section--description">
                         Strong foundation in finance with expertise in quantitative analysis and machine learning. A data science-driven developer with a passion for applying AI solutions to complex financial challenges.
-                        <br /> <br /> 
+                        <br /> <br /> <br /> 
                     </p>
                 </div>
                 <a href="https://www.linkedin.com/in/franklin-chukwuemeka-5514ab150/" target="_blank"><button className="btn btn-primary">Get In Touch</button></a>
