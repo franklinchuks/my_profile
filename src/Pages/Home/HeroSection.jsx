71bc9139c936c42b347f400d696b1a0f2220c63e
franklin <franklinchukwuemeka234@gmail.com>
2024-11-05 09:14:29 +0200

    neww

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
                         Software Development
                     </h3>
                     <p className="hero--section--description">
-                        With a robust background in applied mathematics, statistical modeling, and computer programming, I specialize in developing and implementing quantitative models and algorithms that enhance strategic decision-making and optimize operational strategies.
+                        With a robust background in applied mathematics, statistical modeling, and computer programming, I specialize in developing quantitative models and algorithms that enhance strategic decision-making and optimize operational strategies.
                     </p>
                 </div>
                 <a href="https://www.linkedin.com/in/franklin-chukwuemeka-5514ab150/" target="_blank"><button className="btn btn-primary">Get In Touch</button></a>
