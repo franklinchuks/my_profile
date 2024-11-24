da81b8e59af7364e234ae4e533e3349bc3ea2d83
franklin <franklinchukwuemeka234@gmail.com>
2024-11-24 08:36:54 +0200

    f

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,16 +3,18 @@ export default function HeroSection() {
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
-                    <h3 className="hero--section--title">
-                        <span className="hero--section--title--color">Quant</span>{""}itative Analysis,
-                        <br />
-                        Software Development
-                    </h3>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section--title--color">Hello</span>,<br></br>I'm Franklin.
+                    </h1>
+                    <h2 className="hero--section--title--small">
+                        I use quantitative and programming skills to transform data into actionable insights.
+                    </h2>
+                </div>
+                <a href="https://www.linkedin.com/in/franklin-chukwuemeka-5514ab150/" target="_blank"><button className="btn btn-primary">Get In Touch</button>
+                </a>
                     <p className="hero--section--description">
                         With a robust background in applied mathematics, statistical modeling, and computer programming, I specialize in developing quantitative models and algorithms that enhance strategic decision-making and optimize operational strategies.
                     </p>
-                </div>
-                <a href="https://www.linkedin.com/in/franklin-chukwuemeka-5514ab150/" target="_blank"><button className="btn btn-primary">Get In Touch</button></a>
             </div>
             <div className="hero--section--img">
                 <img src="./img/wallpaper.jpg" alt="Hero Section" />
