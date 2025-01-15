80c8b1f9c63253bc4e2bae8f6611f5835011754f
franklin <franklinchukwuemeka234@gmail.com>
2025-01-15 18:36:34 -0500

    f

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -8,7 +8,7 @@ export default function ContactMe() {
             </div>
             <div class="card-body">
                 <p>Email: franklinchukwuemeka234@gmail.com</p>
-                <p>Phone: +372 5830 0891</p>
+                <p>Phone: +1-2034101323</p>
                 <p>Linkedin: https://www.linkedin.com/in/franklin-chukwuemeka-5514ab150/</p>
                 <p>Website: https://franklinchukwuemeka.netlify.app/</p>
                 <p>Git Repositories: https://github.com/franklinchuks?tab=repositories</p>
