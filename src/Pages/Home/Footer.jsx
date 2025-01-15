e49def24e133e7b6338771b257ac0c895a2cc463
franklin <franklinchukwuemeka234@gmail.com>
2025-01-15 18:33:28 -0500

    f

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -169,7 +169,7 @@ function Footer() {
       </div>
       <hr className="divider" />
       <div className="footer--content--container">
-        <p className="footer--content">Made by Frankie 😏😏</p>
+        <p className="footer--content">Coded from scratch by Franklin using React.Js (2025)</p>
         <div className="footer--social--icon">
           <ul>
             <li>
